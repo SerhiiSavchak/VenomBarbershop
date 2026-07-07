095f7c9b1e8cc41c1871a091618f0fca37c8acfd
Serhii Savchak <itsavchak@gmail.com>
2026-07-08 00:57:31 +0300

    add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Bebas_Neue, Montserrat, Geist } from "next/font/google";
 import { I18nProvider } from "@/components/providers/I18nProvider";
 import "./globals.css";
+
+const GTM_ID = "GTM-5GCQJ75F";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,7 +47,25 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${geistSans.variable} ${montserrat.variable} ${bebasNeue.variable} bg-background`}
     >
+      {/* Google Tag Manager: strategy="beforeInteractive" инжектит скрипт в <head> */}
+      <Script id="gtm" strategy="beforeInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+      </Script>
       <body className="min-h-screen bg-background font-[family-name:var(--font-montserrat)] text-foreground antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <I18nProvider>{children}</I18nProvider>
       </body>
     </html>
